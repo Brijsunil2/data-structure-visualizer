d7a07eeb43bdd11c0d81b4d17ac3631d50b1fb9e
Sunil Brijlall <brijsunil2@gmail.com>
2025-04-15 21:43:15 -0400

    add(DataStructurePage): initial main page for data structures

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import Header from "./components/Header/Header"
+import Header from "./components/Header/Header";
+import DataStructurePage from "./pages/DataStructurePage/DataStructurePage";
 
 const App = () => {
   return (
     <main className="app wrapper-width-md wrapper-width-xl">
       <Header />
+      <DataStructurePage />
     </main>
   )
 }
---
+++ src/pages/DataStructurePage/DataStructurePage.jsx
@@ -0,0 +1,9 @@
+import "./DataStructurePage.scss"
+
+const DataStructurePage = () => {
+  return (
+    <div className="data-structure-page wrapper-width-xl wrapper-width-md">DataStructurePage</div>
+  )
+}
+
+export default DataStructurePage
